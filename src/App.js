618dbb159524e1298bb9f862c6851146d8f3faa0
Armaan <indaniarmaan4@gmail.com>
2024-03-08 09:33:27 +0530

    Animation changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <body>
-        <div className="cooking-img-div">
+        <div className="cooking-text-div">
           <h1>DISCOVER THE WORLD OF</h1>
           <h1>FUSION COOKING</h1>
           <p>UNRAVEL THE WONDERS OF VEGETARIAN COOKING WITH US!</p>
